38e5f4b9803b7003b61529a4d6eed78e0f8bac8c
raenkumi <^p4D3xj2@&p!i#TeP!N9b2nCK#P4APcNo*HhY8F!2%b9ZyUY$7b>
2022-02-07 21:06:20 -0300

    quizz stuff

---
+++ animation/quizz.js
@@ -0,0 +1,10 @@
+var jsonObject = {
+    "question": {
+        "id": "0",
+        "question": "Pergunta 1",
+        "answers": {
+            "0": "Resposta A",
+            "1": "Resposta B"
+        }
+    }
+    }
